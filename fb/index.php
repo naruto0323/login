dce6cc4bbd9fe9b5f63fb5b0caf0b7af36ea1b73
naruto0323 <ramachandra.0323@gmail.com>
2015-06-19 12:28:05 +0530

    facebook login updated

--- fb/index.php
+++ fb/index.php
@@ -1,104 +1,102 @@
-
 <?php
-/**
- * Copyright 2011 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
-require 'src/facebook.php';
-
-// Create our Application instance (replace this with your appId and secret).
+//Application Configurations
+$app_id   = "1415466918780788";
+$app_secret = "196b078bca775181b2e242eec1853a0d";
+$site_url = "http://localhost/fb/index.php";
+ 
+try{
+  require_once "src/Facebook/facebook.php";
+}catch(Exception $e){
+  error_log($e);
+}
+// Create our application instance
 $facebook = new Facebook(array(
-  'appId'  => '1415466918780788',
-  'secret' => '196b078bca775181b2e242eec1853a0d',
-));
-
+  'appId'   => $app_id,
+  'secret'  => $app_secret,
+  ));
+ 
 // Get User ID
 $user = $facebook->getUser();
-
-// We may or may not have this data based on whether the user is logged in.
-//
-// If we have a $user id here, it means we know the user is logged into
-// Facebook, but we don't know if the access token is valid. An access
+// We may or may not have this data based
+// on whether the user is logged in.
+// If we have a $user id here, it means we know
+// the user is logged into
+// Facebook, but we don’t know if the access token is valid. An access
 // token is invalid if the user logged out of Facebook.
-
-if ($user) {
-  try {
+ 
+if($user){
+//==================== Single query method ======================================
+  try{
     // Proceed knowing you have a logged in user who's authenticated.
     $user_profile = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
+  }catch(FacebookApiException $e){
     error_log($e);
-    $user = null;
+    $user = NULL;
+  }
+//==================== Single query method ends =================================
+}
+ 
+if($user){
+  // Get logout URL
+  $logoutUrl = $facebook->getLogoutUrl();
+}else{
+  // Get login URL
+  $loginUrl = $facebook->getLoginUrl(array(
+    'scope'   => 'read_stream, publish_stream, user_birthday, user_location, user_work_history, user_hometown, user_photos',
+    'redirect_uri'  => $site_url,
+    ));
+}
+ 
+if($user){
+  // Proceed knowing you have a logged in user who has a valid session.
+ 
+//========= Batch requests over the Facebook Graph API using the PHP-SDK ========
+  // Save your method calls into an array
+  $queries = array(
+    array('method' => 'GET', 'relative_url' => '/'.$user),
+    array('method' => 'GET', 'relative_url' => '/'.$user.'/home?limit=50'),
+    array('method' => 'GET', 'relative_url' => '/'.$user.'/friends'),
+    array('method' => 'GET', 'relative_url' => '/'.$user.'/photos?limit=6'),
+    );
+ 
+  // POST your queries to the batch endpoint on the graph.
+  try{
+    $batchResponse = $facebook->api('?batch='.json_encode($queries), 'POST');
+  }catch(Exception $o){
+    error_log($o);
+  }
+ 
+  //Return values are indexed in order of the original array, content is in ['body'] as a JSON
+  //string. Decode for use as a PHP array.
+  $user_info    = json_decode($batchResponse[0]['body'], TRUE);
+  $feed     = json_decode($batchResponse[1]['body'], TRUE);
+  $friends_list   = json_decode($batchResponse[2]['body'], TRUE);
+  $photos     = json_decode($batchResponse[3]['body'], TRUE);
+//========= Batch requests over the Facebook Graph API using the PHP-SDK ends =====
+ 
+  // Update user's status using graph api
+  if(isset($_POST['publish'])){
+    try{
+      $publishStream = $facebook->api("/$user/feed", 'post', array(
+        'message'   => 'Check out my new login',
+        'link'      => 'http://localhost',
+        'picture'   => 'http://localhost',
+        'name'      => 'login page',
+        'caption'   => 'fblogin',
+        'description'   => 'A test for facebook login.',
+        ));
+    }catch(FacebookApiException $e){
+      error_log($e);
+    }
+  }
+ 
+  // Update user's status using graph api
+  if(isset($_POST['status'])){
+    try{
+      $statusUpdate = $facebook->api("/$user/feed", 'post', array('message'=> $_POST['status']));
+    }catch(FacebookApiException $e){
+      error_log($e);
+    }
   }
 }
-
-// Login or logout url will be needed depending on current user state.
-if ($user) {
-  $logoutUrl = $facebook->getLogoutUrl();
-} else {
-  $loginUrl = $facebook->getLoginUrl();
-}
-
-// This call will always work since we are fetching public data.
-$naitik = $facebook->api('/naitik');
-
 ?>
-<!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>php-sdk</title>
-    <style>
-      body {
-        font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
-      }
-      h1 a {
-        text-decoration: none;
-        color: #3b5998;
-      }
-      h1 a:hover {
-        text-decoration: underline;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>php-sdk</h1>
-
-    <?php if ($user): ?>
-      <a href="<?php echo $logoutUrl; ?>">Logout</a>
-    <?php else: ?>
-      <div>
-        Login using OAuth 2.0 handled by the PHP SDK:
-        <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
-      </div>
-    <?php endif ?>
-
-    <h3>PHP Session</h3>
-    <pre><?php print_r($_SESSION); ?></pre>
-
-    <?php if ($user): ?>
-      <h3>You</h3>
-      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-
-      <h3>Your User Object (/me)</h3>
-      <pre><?php print_r($user_profile); ?></pre>
-    <?php else: ?>
-      <strong><em>You are not Connected.</em></strong>
-    <?php endif ?>
-
-    <h3>Public profile of Naitik</h3>
-    <img src="https://graph.facebook.com/naitik/picture">
-    <?php echo $naitik['name']; ?>
-  </body>
-</html>
-
